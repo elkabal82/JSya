2f6b22b545319ee0fdddd012e981fe45ae9ac3c9
elkabal82 <etvb66@hotmail.com>
2014-08-19 06:46:18 -0500

    segundo commit
    hacemos el ejercicio 2 de variables

--- 2-variables/js/variables.js
+++ 2-variables/js/variables.js
@@ -1,1 +1,6 @@
-alert("hola")
+//definir uan variable en al cual almacenamos el nombre y otra donde almacenamos el sueldo
+var nombre = "Edgardo Torres";
+var sueldo = 1500;
+
+document.write(nombre + "<br>");
+document.write("$" + sueldo);
