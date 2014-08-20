788c87f1397d8ebed9c8fdecdd9401826b649b2d
elkabal82 <etvb66@hotmail.com>
2014-08-20 11:47:44 -0500

    creamos el ejercicio prompt

---
+++ 2-nombre-edad/js/nombre.js
@@ -0,0 +1,2 @@
+document.write("Nombre: Edgardo Torres Vanden Bouch <br>");
+document.write("Edad: 32")
---
+++ 4-prompt/js/prompt.js
@@ -0,0 +1,9 @@
+//entra por teclado con el prompt
+var nombre;
+var correo;
+
+nombre = prompt("Cual es tu nombre");
+correo = prompt("Cual es tu correo");
+
+document.write("Nombre: " + nombre + "<br>");
+document.write("Correo: " + correo);
